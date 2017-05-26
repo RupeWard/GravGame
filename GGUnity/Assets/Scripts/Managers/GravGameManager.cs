66f4a19bda446c7616e9b2ab18529d7440f33558
RupeWard <rupe@rupertward.co.uk>
2017-05-26 01:57:32 +0100

    GameManager. Fix controller object in game scene

---
+++ GGUnity/Assets/Scripts/Managers/GravGameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RJWS.GravGame
+{
+	public class GravGameManager : RJWS.Core.Singleton.SingletonApplicationLifetimeLazy<GravGameManager>
+	{
+	}
+}
